7981d4f987dd7f0bb05029ab9281602b61db3e30
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-12-16 15:21:35 -0300

    projetcts and imgs

---
+++ data/projects.ts
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    id: 1,
+    imageUrl: "/portfolio.png",
+    nameKey: "projects.portfolio.name",
+    descriptionKey: "projects.portfolio.description",
+    href: "personal-portfolio",
+    tags: ["tags.branding", "tags.ux", "tags.prototyping", "tags.programming"],
+  },
+  {
+    id: 2,
+    imageUrl: "/reverb.png",
+    nameKey: "projects.reverb.name",
+    descriptionKey: "projects.reverb.description",
+    href: "reverb",
+    tags: ["tags.branding", "tags.ux", "tags.prototyping"],
+  },
+];
