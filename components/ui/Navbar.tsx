28234058eb13ea8177803f867ba2f5f1d7be5953
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-12-19 15:58:26 -0300

    feat: responsivity

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -3,45 +3,56 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { Link } from "../../i18n/routing";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { usePathname } from "next/navigation";
 
-import ChangeLanguageSelect from "../ChangeLanguageSelect";
-import { Mail } from "lucide-react";
+import { Menu, X } from "lucide-react";
+import ContactButton from "./ContactButton";
 
 interface NavbarProps {}
 
 const Navbar: FunctionComponent<NavbarProps> = () => {
   const t = useTranslations("NavbarLinks");
 
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="sticky top-0 w-full flex flex-row gap-4 sm:items-center justify-between px-6 xl:px-24 pt-4 pb-2 bg-background">
-      <Link href={`/`} className="hidden sm:block lg:w-full">
-        <Image
-          priority
-          src="/logo.png"
-          alt="jm (joão matos) logo"
-          className=""
-          width={54}
-          height={46}
-        />
-      </Link>
-      <nav className="flex gap-2">
-        <NavLink href={`work`} label={t("work")} />
-        <NavLink href={`about`} label={t("about")} />
-      </nav>
-      <div className="lg:w-full lg:justify-end flex flex-col lg:flex-row items-center gap-2">
-        <a
-          href="mailto:victor.matos.costa.dev@gmail.com"
-          className="p-2 font-black text-background bg-primary rounded-lg"
+    <>
+      <header className="sticky top-0 w-full px-6 pt-4 pb-2 flex flex-row gap-4 sm:items-center justify-between bg-background bg-opacity-30 backdrop-filter backdrop-blur-lg">
+        <Link href={`/#Hi!`}>
+          <Image
+            priority
+            src="/logo.png"
+            alt="jm (joão matos) logo"
+            className=""
+            width={54}
+            height={46}
+          />
+        </Link>
+        <nav className="hidden sm:flex gap-4 items-center">
+          <NavLink href={`Work`} label={t("work")} />
+          <NavLink href={`About`} label={t("about")} />
+          <ContactButton />
+        </nav>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="block sm:hidden text-primary"
         >
-          <button className="flex items-center gap-2">
-            <Mail /> {t("contact-call")}
-          </button>
-        </a>
-        <ChangeLanguageSelect />
-      </div>
-    </header>
+          {isOpen ? <X size={48} /> : <Menu size={48} />}
+        </button>
+      </header>
+      {isOpen && (
+        <nav className="sm:hidden w-full fixed top-20 flex flex-col p-6 gap-2 bg-background">
+          <NavLink href={`Work`} label={t("work")} />
+          <NavLink href={`About`} label={t("about")} />
+          <a
+            href="mailto:victor.matos.costa.dev@gmail.com"
+            className="font-black text-xl"
+          >
+            <button>{t("contact-call")}</button>
+          </a>
+        </nav>
+      )}
+    </>
   );
 };
 
@@ -53,15 +64,10 @@ interface NavLinkProps {
 }
 
 const NavLink: FunctionComponent<NavLinkProps> = ({ href, label }) => {
-  const pathname = usePathname();
-  const isActive = pathname.split("/")[2] === href;
-
   return (
     <Link
-      href={`/${href}`}
-      className={`text-xl font-black ${
-        isActive ? "text-foreground" : "text-foreground/60"
-      }`}
+      href={`/#${href}`}
+      className="text-xl text-foreground/60 hover:text-foreground transition-colors duration-150"
     >
       {label}
     </Link>
