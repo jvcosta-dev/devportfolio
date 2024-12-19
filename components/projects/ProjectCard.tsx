24188ce6a9542713908d1fb3322c7ce73224451b
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-12-19 15:59:32 -0300

    layout changes

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -13,30 +13,34 @@ interface Props {
 const ProjectCard: FunctionComponent<Props> = ({ project }) => {
   const t = useTranslations();
   return (
-    <div className="flex flex-col gap-2">
-      <Image
-        alt={`${t(project.nameKey)} img`}
-        src={project.imageUrl}
-        width={384}
-        height={384}
-        className="object-cover w-auto h-auto"
-      />
-      <div>
-        {project.tags.map((tag, i) => (
-          <span key={i} className="text-foreground/60">
-            {t(tag)} {i !== project.tags.length - 1 && "/ "}
-          </span>
-        ))}
+    <Link
+      href={`/work/${project.href}`}
+      className="w-full flex flex-col-reverse md:flex-row md:items-center md:gap-6 md:pl-12 rounded-xl"
+      style={{ background: project.bgColor, color: project.color || "inherit" }}
+    >
+      <div className="flex flex-col gap-2 p-4 md:p-0">
+        <div>
+          {project.tags.map((tag, i) => (
+            <span key={i} className="text-inherit">
+              {t(`tags.${tag}`)} {i !== project.tags.length - 1 && "/ "}
+            </span>
+          ))}
+        </div>
+        <h2 className="w-72 text-3xl font-black">
+          {t(`projects.${project.name}.name`)}
+        </h2>
+
+        <p>{t(`projects.${project.name}.description`)}</p>
       </div>
-      <h2 className="text-2xl font-black">{t(project.nameKey)}</h2>
-      <Link
-        href={`/work/${project.href}`}
-        className="w-max flex items-center gap-2 text-primary"
-      >
-        <u className="text-xl">{t("projects.link")}</u>
-        <LinkIcon />
-      </Link>
-    </div>
+      <Image
+        alt={`${project.name}.name img`}
+        src={project.imageUrl}
+        width={1280}
+        height={768}
+        priority
+        className="bg-slate-400 w-full h-64 md:h-80 xl:h-96 aspect-auto rounded-xl object-cover"
+      />
+    </Link>
   );
 };
 
