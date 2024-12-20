8fc4225a9b11ba09b587e7139073344f2b129445
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-12-20 13:48:57 -0300

    fix: styling + projects data

--- components/ui/Page.tsx
+++ components/ui/Page.tsx
@@ -6,7 +6,7 @@ interface PageProps {
 
 const Page: FunctionComponent<PageProps> = ({ children }) => {
   return (
-    <main className="max-w-7xl mx-auto flex flex-col flex-grow px-6 gap-10 xl:gap-20 mt-10 xl:mt-20 pb-10 xl:pb-20">
+    <main className="max-w-7xl mx-auto flex flex-col flex-grow px-6 gap-16 xl:gap-20 mt-10 xl:mt-20 pb-16 xl:pb-20">
       {children}
     </main>
   );
--- data/projects.ts
+++ data/projects.ts
@@ -1,14 +1,6 @@
 export const projects = [
   {
     id: 1,
-    imageUrl: "/portfolio.png",
-    name: "portfolio",
-    href: "personal-portfolio",
-    tags: ["branding", "ux", "prototyping", "programming"],
-    bgColor: "#F0F0F0",
-  },
-  {
-    id: 2,
     imageUrl: "/reverb.png",
     name: "reverb",
     href: "reverb",
