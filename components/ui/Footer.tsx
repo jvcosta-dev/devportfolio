d925ab7f3c0bbf6c1705cffae5479bfd720a3b10
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-12-20 13:49:56 -0300

    fix: link icons

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,7 +1,5 @@
 import { FunctionComponent } from "react";
-import { Link } from "../../i18n/routing";
 import Image from "next/image";
-import { Github, Mail, Phone } from "lucide-react";
 import ChangeLanguageSelect from "../ChangeLanguageSelect";
 
 interface FooterProps {}
@@ -11,29 +9,46 @@ const Footer: FunctionComponent<FooterProps> = () => {
     <footer className="w-full flex flex-col gap-4 justify-between px-6 pt-4 pb-2 bg-background">
       <div className="flex items-center justify-between">
         <div className="flex gap-4">
-          <a
-            href="https://github.com/jvcosta-dev"
-            target="_blank"
-            className="p-2 rounded-full bg-slate-200 hover:bg-black hover:text-background"
-            aria-label="jvcosta-dev github"
-          >
-            <Github />
-          </a>
-          <a
-            href="mailto:victor.matos.costa.dev@gmail.com"
-            className="p-2 rounded-full bg-slate-200 hover:bg-primary hover:text-background"
-            aria-label="mail to victor.matos.costa.dev@gmail.com"
-          >
-            <Mail />
-          </a>
-          <a
-            href="https://wa.me/5511984237631"
-            target="_blank"
-            className="p-2 rounded-full bg-slate-200 hover:bg-green-600 hover:text-background"
-            aria-label="whatsapp"
-          >
-            <Phone />
-          </a>
+          <abbr title="Github">
+            <a href="https://github.com/jvcosta-dev" target="_blank">
+              <Image
+                src={"/social-icons/github.webp"}
+                alt="github icon"
+                width={32}
+                height={32}
+                className="h-8 w-8 object-contain"
+              />
+            </a>
+          </abbr>
+          <abbr title="Email">
+            <a
+              href="mailto:victor.matos.costa.dev@gmail.com"
+              aria-label="mail to victor.matos.costa.dev@gmail.com"
+            >
+              <Image
+                src={"/social-icons/gmail.png"}
+                alt="gmail icon"
+                width={32}
+                height={32}
+                className="h-8 w-8 object-contain"
+              />
+            </a>
+          </abbr>
+          <abbr title="Whatsapp">
+            <a
+              href="https://wa.me/5511984237631"
+              target="_blank"
+              aria-label="whatsapp"
+            >
+              <Image
+                src={"/social-icons/whatsapp.png"}
+                alt="whatsapp icon"
+                width={32}
+                height={32}
+                className="h-8 w-8 object-contain"
+              />
+            </a>
+          </abbr>
         </div>
         <ChangeLanguageSelect />
       </div>
